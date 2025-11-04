0dedd1c313ca23b24d9de8999c704c5009dbf668
kryzkoff <kryzruiz10@gmail.com>
2025-11-04 18:22:00 -0500

    cola kryz

--- Clases/Cola.cs
+++ Clases/Cola.cs
@@ -1,1 +1,81 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clases
+{
+    public class Cola
+    {
+        private NodoCola frente;
+        private NodoCola fin;
+
+        public Cola()
+        {
+            frente = null;
+            fin = null;
+        }
+
+        public void Encolar(Paciente p)
+        {
+            var nuevo = new NodoCola(p);
+            if (frente == null)
+            {
+                frente = nuevo;
+                fin = nuevo;
+            }
+            else
+            {
+                if (p.Prioridad == 0)
+                {
+                    fin.Siguiente = nuevo;
+                    fin = nuevo;
+                }
+                else
+                {
+                    if (frente.Dato.Prioridad == 0)
+                    {
+                        nuevo.Siguiente = frente;
+                        frente = nuevo;
+                    }
+                    else
+                    {
+                        NodoCola temp = frente;
+                        while (temp.Siguiente != null && temp.Siguiente.Dato.Prioridad == 1)
+                        {
+                            temp = temp.Siguiente;
+                        }
+                        nuevo.Siguiente = temp.Siguiente;
+                        temp.Siguiente = nuevo;
+                        if (nuevo.Siguiente == null)
+                            fin = nuevo;
+                    }
+                }
+            }
+        }
+
+        public Paciente Desencolar()
+        {
+            if (frente == null) return null;
+            Paciente p = frente.Dato;
+            frente = frente.Siguiente;
+            if (frente == null) fin = null;
+            return p;
+        }
+        public List<Paciente> ObtenerTodosLosPacientes()
+        {
+            var lista = new List<Paciente>();
+            var actual = frente;
+
+            while (actual != null)
+            {
+                lista.Add(actual.Dato);
+                actual = actual.Siguiente;
+            }
+
+            return lista;
+        }
+        public bool TienePacientes()
+        {
+            return frente != null;
+        }
+    }
+}
